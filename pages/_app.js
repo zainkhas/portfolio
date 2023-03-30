1d59d7bc68451f3f3237bb6c42c1d758f807f1bf
Zain Ul Abdin <zainkhas93@gmail.com>
2023-03-31 02:07:42 +0500

    Enabled vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import GoogleAnalyticsWrapper from "../components/GoogleAnalyticsWrapper";
 import "../styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }) {
   return (
     <GoogleAnalyticsWrapper>
       <Component {...pageProps} />
+      <Analytics />
     </GoogleAnalyticsWrapper>
   );
 }

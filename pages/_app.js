83a5e07e723b868d2d35e53f103404911528cb08
Zain Ul Abdin <zainkhas93@gmail.com>
2023-04-06 21:23:54 +0500

    Changes for GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,16 @@
-import GoogleAnalyticsWrapper from "../components/GoogleAnalyticsWrapper";
+import { useEffect } from "react";
 import "../styles/globals.css";
-import { Analytics } from "@vercel/analytics/react";
+import { initGA, logPageView } from "../utils/analytics";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <GoogleAnalyticsWrapper>
-      <Component {...pageProps} />
-      <Analytics />
-    </GoogleAnalyticsWrapper>
-  );
+  useEffect(() => {
+    if (!window.GA_INITIALIZED) {
+      initGA();
+      window.GA_INITIALIZED = true;
+    }
+    logPageView();
+  }, []);
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;

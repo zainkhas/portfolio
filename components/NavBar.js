93b1e2d5e2ff29c0ae03121ad798393e4cc76814
Zain Ul Abdin <zainkhas93@gmail.com>
2023-04-06 21:57:46 +0500

    GA integration and fixes

--- components/NavBar.js
+++ components/NavBar.js
@@ -6,51 +6,51 @@ const NavBar = ({ menu }) => {
   return (
     <>
       <div>
-        <nav class="border-gray-200">
-          <div class="container mx-auto flex flex-wrap items-center justify-between py-8">
-            <a href="#" class="flex"></a>
+        <nav className="border-gray-200">
+          <div className="container mx-auto flex flex-wrap items-center justify-between py-8">
+            <a href="#" className="flex"></a>
             <button
               data-collapse-toggle="mobile-menu"
               type="button"
-              class="md:hidden ml-3 text-gray-400 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-400 rounded-lg inline-flex items-center justify-center"
+              className="md:hidden ml-3 text-gray-400 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-400 rounded-lg inline-flex items-center justify-center"
               aria-controls="mobile-menu-2"
               aria-expanded="false"
             >
-              <span class="sr-only">Open main menu</span>
+              <span className="sr-only">Open main menu</span>
               <svg
-                class="w-6 h-6"
+                className="w-6 h-6"
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 ></path>
               </svg>
               <svg
-                class="hidden w-6 h-6"
+                className="hidden w-6 h-6"
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 ></path>
               </svg>
             </button>
-            <div class="hidden md:block w-full md:w-auto" id="mobile-menu">
-              <ul class="flex-col md:flex-row flex md:space-x-8 mt-4 md:mt-0 md:text-sm md:font-medium">
+            <div className="hidden md:block w-full md:w-auto" id="mobile-menu">
+              <ul className="flex-col md:flex-row flex md:space-x-8 mt-4 md:mt-0 md:text-sm md:font-medium">
                 {menu
                   .filter((item) => !item.hide)
                   .map((item) => (
                     <li key={item.name}>
                       <Link
                         href={item.page}
-                        class="text-gray-700 hover:text-black hover:bg-gray-100  md:hover:bg-transparent border-0 block pl-3 pr-4 py-2 md:p-0"
+                        className="text-gray-700 hover:text-black hover:bg-gray-100  md:hover:bg-transparent border-0 block pl-3 pr-4 py-2 md:p-0"
                       >
                         {item.name}
                       </Link>
--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,25 @@
-import { useEffect } from "react";
+import Script from "next/script";
 import "../styles/globals.css";
-import { initGA, logPageView } from "../utils/analytics";
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    if (!window.GA_INITIALIZED) {
-      initGA();
-      window.GA_INITIALIZED = true;
-    }
-    logPageView();
-  }, []);
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_KEY}`}
+      ></Script>
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_KEY}');`,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
